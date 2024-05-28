9dd749b19903143f8df6933adfa0c83790bf43f6
moony404 <theo.marliac63@gmail.com>
2024-05-28 18:30:09 +0200

    update

--- maths.c
+++ maths.c
@@ -5,6 +5,7 @@
 
 int main(int argc, char *argv[])
 {
+	unsigned char i = 1;
 	int sw = 0;
 	float ab = 0;
 	float ab_nosqrt = 0;
@@ -19,45 +20,47 @@ int main(int argc, char *argv[])
 	printf("2. Longueur d'un segment\n");
 	while (i==1) {
 		printf("here\n");
-	}
-	scanf("%d",&sw);
-	switch (sw) {
-		case 1:
-			printf("Valeur décimale de x_a :\n"); //x_a
-			scanf("%f",&x_a);
-			printf("Valeur décimale de y_a :\n"); //y_a
-			scanf("%f",&y_a);
-			printf("Valeur décimale de x_b :\n"); //x_b
-			scanf("%f",&x_b);
-			printf("Valeur décimale de y_b :\n"); //y_b
-			scanf("%f",&y_b);
-			printf("a(%f;%f)\n",x_a,y_a); //coordonnées de a
-			printf("b(%f;%f)\n",x_b,y_b); //coordonnées de b
-			//calcul du résultat
-			//soit k le milieu de [AB]
-			x_k = (x_a+x_b)/2;
-			y_k = (y_a+y_b)/2;
-			printf("k(%f;%f)\n",x_k,y_k);
-		break;
-		case 2:
-			printf("");
-			printf("Valeur décimale de x_a :\n"); //x_a
-			scanf("%f",&x_a);
-			printf("Valeur décimale de y_a :\n"); //y_a
-			scanf("%f",&y_a);
-			printf("Valeur décimale de x_b :\n"); //x_b
-			scanf("%f",&x_b);
-			printf("Valeur décimale de y_b :\n"); //y_b
-			scanf("%f",&y_b);
-			printf("a(%f;%f)\n",x_a,y_a); //coordonnées de a
-			printf("b(%f;%f)\n",x_b,y_b); //coordonnées de b
-			//calcul
-			ab_nosqrt = (x_b+x_a)*(x_b+x_a)-(y_b+y_a)*(y_b+y_a);
-			ab = sqrt(ab_nosqrt);
-			printf("%f",ab);
-		break;
-		default:
-			printf("Error, you didn't use a correct number or a inexepted error occured\n");
-		break;
+		scanf("%d",&sw);
+		switch (sw) {
+			case 1:
+				printf("Valeur décimale de x_a :\n"); //x_a
+				scanf("%f",&x_a);
+				printf("Valeur décimale de y_a :\n"); //y_a
+				scanf("%f",&y_a);
+				printf("Valeur décimale de x_b :\n"); //x_b
+				scanf("%f",&x_b);
+				printf("Valeur décimale de y_b :\n"); //y_b
+				scanf("%f",&y_b);
+				printf("a(%f;%f)\n",x_a,y_a); //coordonnées de a
+				printf("b(%f;%f)\n",x_b,y_b); //coordonnées de b
+				//calcul du résultat
+				//soit k le milieu de [AB]
+				x_k = (x_a+x_b)/2;
+				y_k = (y_a+y_b)/2;
+				printf("k(%f;%f)\n",x_k,y_k);
+			break;
+			case 2:
+				printf("");
+				printf("Valeur décimale de x_a :\n"); //x_a
+				scanf("%f",&x_a);
+				printf("Valeur décimale de y_a :\n"); //y_a
+				scanf("%f",&y_a);
+				printf("Valeur décimale de x_b :\n"); //x_b
+				scanf("%f",&x_b);
+				printf("Valeur décimale de y_b :\n"); //y_b
+				scanf("%f",&y_b);
+				printf("a(%f;%f)\n",x_a,y_a); //coordonnées de a
+				printf("b(%f;%f)\n",x_b,y_b); //coordonnées de b
+				//calcul
+				ab_nosqrt = (x_b+x_a)*(x_b+x_a)-(y_b+y_a)*(y_b+y_a);
+				ab = sqrt(ab_nosqrt);
+				double(ab);
+				// ab = sqrt
+				printf("%f",ab);
+			break;
+			default:
+				printf("Error, you didn't use a correct number or a inexepted error occured\n");
+			break;
+		}
 	}
 }
