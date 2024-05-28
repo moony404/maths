76249f6ef13da6bf02196e0e72d6b6900cd2d6d2
moony404 <theo.marliac63@gmail.com>
2024-05-28 18:33:24 +0200

    updated the code with rthe while (it was in the previous commit but i say it on that one) + changed all of the "é" to "e"

--- maths.c
+++ maths.c
@@ -15,25 +15,24 @@ int main(int argc, char *argv[])
 	float y_a = 0;
 	float x_b = 0;
 	float y_b = 0;
-	printf("######## maths ########\n");
-	printf("1. Coordonées k du milieu d'un segment\n");
-	printf("2. Longueur d'un segment\n");
 	while (i==1) {
-		printf("here\n");
+		printf("######## maths ########\n");
+		printf("1. Coordonees k du milieu d'un segment\n");
+		printf("2. Longueur d'un segment\n");
 		scanf("%d",&sw);
 		switch (sw) {
 			case 1:
-				printf("Valeur décimale de x_a :\n"); //x_a
+				printf("Valeur decimale de x_a :\n"); //x_a
 				scanf("%f",&x_a);
-				printf("Valeur décimale de y_a :\n"); //y_a
+				printf("Valeur decimale de y_a :\n"); //y_a
 				scanf("%f",&y_a);
-				printf("Valeur décimale de x_b :\n"); //x_b
+				printf("Valeur decimale de x_b :\n"); //x_b
 				scanf("%f",&x_b);
-				printf("Valeur décimale de y_b :\n"); //y_b
+				printf("Valeur decimale de y_b :\n"); //y_b
 				scanf("%f",&y_b);
-				printf("a(%f;%f)\n",x_a,y_a); //coordonnées de a
-				printf("b(%f;%f)\n",x_b,y_b); //coordonnées de b
-				//calcul du résultat
+				printf("a(%f;%f)\n",x_a,y_a); //coordonnees de a
+				printf("b(%f;%f)\n",x_b,y_b); //coordonnees de b
+				//calcul du resultat
 				//soit k le milieu de [AB]
 				x_k = (x_a+x_b)/2;
 				y_k = (y_a+y_b)/2;
@@ -41,16 +40,16 @@ int main(int argc, char *argv[])
 			break;
 			case 2:
 				printf("");
-				printf("Valeur décimale de x_a :\n"); //x_a
+				printf("Valeur decimale de x_a :\n"); //x_a
 				scanf("%f",&x_a);
-				printf("Valeur décimale de y_a :\n"); //y_a
+				printf("Valeur decimale de y_a :\n"); //y_a
 				scanf("%f",&y_a);
-				printf("Valeur décimale de x_b :\n"); //x_b
+				printf("Valeur decimale de x_b :\n"); //x_b
 				scanf("%f",&x_b);
-				printf("Valeur décimale de y_b :\n"); //y_b
+				printf("Valeur decimale de y_b :\n"); //y_b
 				scanf("%f",&y_b);
-				printf("a(%f;%f)\n",x_a,y_a); //coordonnées de a
-				printf("b(%f;%f)\n",x_b,y_b); //coordonnées de b
+				printf("a(%f;%f)\n",x_a,y_a); //coordonnees de a
+				printf("b(%f;%f)\n",x_b,y_b); //coordonnees de b
 				//calcul
 				ab_nosqrt = (x_b+x_a)*(x_b+x_a)-(y_b+y_a)*(y_b+y_a);
 				ab = sqrt(ab_nosqrt);
